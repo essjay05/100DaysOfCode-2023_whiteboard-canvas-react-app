73f935c419b526deb071ec1fa74900d8efc25e3e
Joy Serquiña <stepjoyous@gmail.com>
2023-03-14 12:14:27 -0700

    Updated styling for tools-wrapper. Added change pen color functionlity successfully.

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,6 +5,7 @@ const Board = () => {
 
   const canvasRef = useRef(null)
   const [penWidth, setPenWidth] = useState(3)
+  const [penColor, setPenColor] = useState('#000000')
   const [fabricCanvas, setFabricCanvas] = useState(3)
 
   useEffect(() => {
@@ -32,12 +33,19 @@ const Board = () => {
     setPenWidth()
   }
 
+  const changePenColor = color => {
+    if (fabricCanvas) {
+      fabricCanvas.freeDrawingBrush.color = color
+      setPenColor(color)
+    }
+  }
+
   return (
     <div className='whiteboard container w-full h-full mx-auto'>
       <h1>Whiteboard</h1>
       <canvas ref={canvasRef}></canvas>
       <section className='tools-container'>
-        <div>
+        <div className='tool-wrapper'>
           <label htmlFor="PenWidth">
             <strong>Pen Width:</strong>&nbsp;{penWidth}
           </label>
@@ -46,6 +54,18 @@ const Board = () => {
             id="PenWidth" 
             onChange={e => changePenWidth(e.target.value)} 
             value={penWidth}
+            min={1}
+            max={30}/>
+        </div>
+        <div className='tool-wrapper'>
+          <label htmlFor="PenColor">
+            <strong>Pen Color:</strong>
+          </label>
+          <input 
+            type="color" 
+            id="PenColor" 
+            onChange={e => changePenColor(e.target.value)} 
+            value={penColor}
             min={1}
             max={30}/>
         </div>
